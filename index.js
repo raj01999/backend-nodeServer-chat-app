9c74476fe4b451157659eb7784c449cba766ecb5
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-04-29 23:33:53 +0530

    Raj

--- index.js
+++ index.js
@@ -27,13 +27,18 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send", (message) => {
-    socket.broadcast.emit("receive", {
-      message: message,
-      name: users[socket.id],
-    });
+    if (users[socket.id]) {
+      socket.broadcast.emit("receive", {
+        message: message,
+        name: users[socket.id],
+      });
+    } else {
+      socket.emit("no-user", message);
+    }
   });
 
   socket.on("disconnect", (message) => {
+    if (!users[socket.id]) return;
     socket.broadcast.emit("left", users[socket.id]);
     delete users[socket.id];
   });
--- router.js
+++ router.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(`<h1>Server is Running... Sarafraj Mallick</h1>`).status(200);
+  res
+    .send(
+      `<h1 style="text-align: center; font-size: 5rem; margin-top: 5rem;"><a href="https://raj01999.github.io/-nodeServer-chat-app/">Click Me</a></h1>`
+    )
+    .status(200);
 });
 
 router.get("/favicon.ico", (req, res) => {
-  res.send(`<h1>Server is Running... Sarafraj Mallick</h1>`).status(200);
+  res
+    .send(
+      `<h1 style="text-align: center; font-size: 5rem; margin-top: 5rem;"><a href="https://raj01999.github.io/-nodeServer-chat-app/">Click Me</a></h1>`
+    )
+    .status(200);
 });
 
 module.exports = router;
