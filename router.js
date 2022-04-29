0a589577d03dc4db9efbce70a23962b908d39b3c
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-04-29 05:52:19 +0530

    done

--- router.js
+++ router.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
+  res.send(`<h1>Server is Running... Sarafraj Mallick</h1>`).status(200);
 });
 
 router.get("/favicon.ico", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
+  res.send(`<h1>Server is Running... Sarafraj Mallick</h1>`).status(200);
 });
 
 module.exports = router;
