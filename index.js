2ace4ba23f4911e56c0c5a8ec6b0554eb44efaef
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-05-02 08:36:14 +0530

    version change

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const socketio = require("socket.io");
 const http = require("http");
+const socketio = require("socket.io");
 const cors = require("cors");
-const router = require("./router");
-
-const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+
 const io = socketio(server, {
   cors: {
     origin: "*",
@@ -15,35 +13,63 @@ const io = socketio(server, {
 });
 
 app.use(cors());
-app.use(router);
+
+app.get("/", (req, res) => {
+  res
+    .send(
+      `<h1 style="text-align: center; font-size: 5rem; margin-top: 5rem;"><a href="https://raj01999.github.io/-nodeServer-chat-app/">Click Me</a></h1>`
+    )
+    .status(200);
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res
+    .send(
+      `<h1 style="text-align: center; font-size: 5rem; margin-top: 5rem;"><a href="https://raj01999.github.io/-nodeServer-chat-app/">Click Me</a></h1>`
+    )
+    .status(200);
+});
 
 const users = {};
 
 io.on("connection", (socket) => {
-  socket.on("new-user-joined", (name) => {
-    users[socket.id] = name;
+  socket.on("new-user-join", (obj) => {
+    const name = obj.name;
+    const time = obj.time;
 
-    socket.broadcast.emit("user-joined", name);
-  });
-
-  socket.on("send", (message) => {
-    if (users[socket.id]) {
-      socket.broadcast.emit("receive", {
-        message: message,
-        name: users[socket.id],
+    if (name) {
+      users[socket.id] = { name, time };
+      socket.broadcast.emit("user-join", {
+        name: name,
+        time: time,
+        users: users,
       });
+
+      socket.emit("no-user", users);
     } else {
-      socket.emit("no-user", message);
+      socket.emit("no-user", users);
     }
   });
 
-  socket.on("disconnect", (message) => {
-    if (!users[socket.id]) return;
-    socket.broadcast.emit("left", users[socket.id]);
+  socket.on("msg-send", (msg) => {
+    const obj = users[socket.id];
+    if (obj) {
+      socket.broadcast.emit("msg-receive", {
+        msg: msg,
+        name: obj.name,
+        users: users,
+      });
+    } else {
+      socket.emit("no-user", users);
+    }
+  });
+
+  socket.on("disconnect", (msg) => {
+    const obj = users[socket.id];
+    if (!obj) return;
     delete users[socket.id];
+    socket.broadcast.emit("left", { name: obj.name, users: users });
   });
 });
 
-server.listen(process.env.PORT || 3000, () =>
-  console.log(`Server has started.`)
-);
+server.listen(process.env.PORT || 80, () => {});
